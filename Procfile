16e6a73e114406f88d654d64a31ca23ca569ac09
steven-king <cujophoto@gmail.com>
2014-02-27 21:29:41 -0500

    adding heroku server stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: gunicorn newschool.wsgi
 web: python manage.py runserver 0.0.0:$PORT --noreload
--- newschool/settings_server.py
+++ newschool/settings_server.py
@@ -4,6 +4,7 @@ import os
 
 PROJECT_ROOT = os.path.dirname(__file__)
 PROJECT_PATH = os.path.dirname(PROJECT_ROOT)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 DEBUG = True
@@ -62,12 +63,16 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
+###STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+###STATIC_URL = '/static/'
 
+
+#heroku vars
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 
 # Additional locations of static files
@@ -161,10 +166,12 @@ LOGGING = {
         },
     }
 }
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
